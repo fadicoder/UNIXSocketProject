35f33698d47b5b8cb338b7d6aa6fa82aad4918e6
Fadi Beirouty <beirouty.fadi@gmail.com>
2024-03-20 15:54:43 -0400

    Quality insurance of client code

--- client/main.cpp
+++ client/main.cpp
@@ -16,35 +16,19 @@ void sigpipeHandler(int signum) {
     exit(1);
 }
 
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cout << "Required argument: path to socket" << std::endl;
-        return 1;
-    }
-    const char* SOCKET_PATH = argv[1];
-
-    signal(SIGPIPE, sigpipeHandler);
-
-    Socket socket;
-    if(!socket.initialize(SOCKET_PATH)){
-        return 1;
-    }
-    if (!socket.connectToServer()){
-        return 1;
-    }
-
+void sendCommandArgs(Socket socket, int argc, char** argv) {
     std::string response;
-    
-    if (argc > 2){
-        for (uint i = 2; i < argc; i++){
-            socket.writeString(argv[i]);
-            socket.readString(&response);
-            std::cout << response << std::endl;
-        }
-        return 0;
+    for (uint i = 2; i < argc; i++){
+        socket.writeString(argv[i]);
+        socket.readString(&response);
+        std::cout << response << std::endl;
     }
+}
+
+void runShell(Socket socket) {
     std::string input;
+    std::string response;
+    std::cout << "Welcome to UNIX socket shell! You can enter a command for the server or press CTRL+C to exit" << std::endl;
     do {
         std::cout << SHELL_LINE_START;
         std::cin >> input;
@@ -52,4 +36,30 @@ int main(int argc, char *argv[]) {
         socket.readString(&response);
         std::cout << response << std::endl;
     } while(true);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "ERROR: 1 required argument: path to socket" << std::endl;
+        return 1;
+    }
+
+    signal(SIGPIPE, sigpipeHandler);
+
+    Socket socket;
+
+    if(!socket.initialize(argv[1])){
+        return 1;
+    }
+
+    if (!socket.connectToServer()){
+        return 1;
+    }
+    
+    if (argc > 2) {
+        sendCommandArgs(socket, argc, argv);
+        return 0;
+    }
+
+    runShell(socket);
 }
