cf3213b610c8c303a61caf6a5bb206ef5b212697
Fadi Beirouty <beirouty.fadi@gmail.com>
2024-03-20 12:28:51 -0400

    Modifying client to support both input and command line arguments

--- client/main.cpp
+++ client/main.cpp
@@ -14,32 +14,23 @@ int main(int argc, char *argv[]) {
     }
     const char* SOCKET_PATH = argv[1];
 
-    // sockaddr_un addr;
-    // char buffer;
-
-    // // Create a new client socket with domain: AF_UNIX, type: SOCK_STREAM, protocol: 0
-    // int sfd = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    // if (sfd == -1) {
-    //     std::cerr << "ERROR: Failed to create socket" << std::endl;
-    // }
-
-    // memset(&addr, 0, sizeof(struct sockaddr_un));
-    // addr.sun_family = AF_UNIX;
-    // strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
-
-    // if (connect(sfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1) {
-    //     std::cerr << "ERROR: Failed to connect to server socket. Make sure the server is running" << std::endl;
-    //     return 1;
-    // }
-
     MySocket serverSocket;
     if(!serverSocket.init(SOCKET_PATH)){
         return 1;
     }
-    std::string input;
     std::string response;
+    
+    if (argc > 2){
+        for (uint i = 2; i < argc; i++){
+            serverSocket.writeString(argv[i]);
+            serverSocket.readString(&response);
+            std::cout << response << std::endl;
+        }
+        return 0;
+    }
+    std::string input;
     do {
+        std::cout << ">>> ";
         std::cin >> input;
         serverSocket.writeString(input);
         serverSocket.readString(&response);
