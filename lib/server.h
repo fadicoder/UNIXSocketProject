b4866824269e9383cd0e32d2fd0ef61bf762db1f
Fadi Beirouty <beirouty.fadi@gmail.com>
2024-03-21 10:47:45 -0400

    Documenting server.h

--- lib/server.h
+++ lib/server.h
@@ -5,14 +5,30 @@
 #include "socketError.h"
 #include "socket.h"
 
+#define DEFAULT_MAX_CONNECTIONS 1
+
 class Server {
 private:
     int fd;
 
 public:
     Server();
+
     ~Server();
-    SocketError start(const char* socketPath, const uint maxConnections);
+
+    /**
+     * This function will start the server application by initializing a UNIX socket in the specified path.
+     * If the path does not exist, it will create a new file, otherwise it will replace the file already present.
+     * The server allows only 1 connection of the maxConnections argument is not specified.
+     * @returns an instance of SocketError representing the error
+    */
+    SocketError start(const char* socketPath, const uint maxConnections = DEFAULT_MAX_CONNECTIONS);
+
+    /**
+     * This function will wait for a new client to connect. It is a blocking function.
+     * The function should be called after start() method.
+     * @returns A pointer to the socket instance used to communicated with the connected client.
+    */
     Socket* acceptClient();
 };
 
